d4b75c6a920e21af4c4bef4200c6e78fb4770ec6
Gunkar16 <76874019+Gunkar16@users.noreply.github.com>
2021-06-16 18:34:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload(){
+
+}
+function setup(){
+    Canvas = createCanvas(500,500);
+    Canvas.position(505,170);
+    Video = createCapture(VIDEO);
+    Video.position(20,170);
+    Video.size(400,400)
+}
